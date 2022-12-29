f20aad5ad440ff996cec45f6a34621a8b3bbc44c
Emmanuel KWENE <mystroken@gmail.com>
2022-12-29 11:09:29 +0100

    fix(gateway): use the local network host 0.0.0.0

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -4,12 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const host = process.env.HOST || "0.0.0.0";
+const port = Number(process.env.PORT) || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
 });
